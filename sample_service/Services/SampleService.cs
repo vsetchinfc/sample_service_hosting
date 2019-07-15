53c2ea651bf2c2c12052a1be80798d9bdd2753e6
Vladislav Setchin <vlad@setchinfc.com.au>
2019-07-15 14:43:37 +1000

    Implemented SampleService as HostedService

--- sample_service/Services/SampleService.cs
+++ sample_service/Services/SampleService.cs
@@ -1,12 +1,32 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace VSC.Services
 {
     public class SampleService : IHostedService, IDisposable
     {
+        IApplicationLifetime _appLifetime;
+        ILogger<SampleService> _logger;
+        IHostingEnvironment _environment;
+        IConfiguration _configuration;
+
+        public SampleService(
+            IConfiguration configuration,
+            IHostingEnvironment environment,
+            ILogger<SampleService> logger, 
+            IApplicationLifetime appLifetime
+        )
+        {
+            _configuration = configuration;
+            _environment = environment;
+            _logger = logger;
+            _appLifetime = appLifetime;
+        }
+
         public void Dispose()
         {
             // TODO: Do service clean up here
@@ -14,16 +34,41 @@ namespace VSC.Services
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            // TODO: Do work here
+            _logger.LogTrace("SampleService StartAsync method called.");
+
+            _appLifetime.ApplicationStarted.Register(OnStarted);
+            _appLifetime.ApplicationStopping.Register(OnStopping);
+            _appLifetime.ApplicationStopped.Register(OnStopped);
 
             return Task.CompletedTask;
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            // stop service processes here
+            _logger.LogTrace("SampleService StopAsync method called.");
             
             return Task.CompletedTask;
+        }
+
+        private void OnStarted()
+        {
+            _logger.LogTrace("SampleService OnStarted method called.");
+
+            // Post-startup code goes here
+        }
+
+        private void OnStopping()
+        {
+            _logger.LogTrace("SampleService OnStopping method called.");
+
+            // On-stopping code goes here
+        }
+
+        private void OnStopped()
+        {
+            _logger.LogTrace("SampleService OnStopped method called.");
+
+            // Post-stopped code goes here
         }
     }
 }
--- sample_service_daemon/Program.cs
+++ sample_service_daemon/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Hosting;
 
 namespace VSC
 {
@@ -6,7 +11,52 @@ namespace VSC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IHostBuilder hostBuilder = CreateHostBuilder(args);
+            if(hostBuilder != null)
+            {
+                IHost host = hostBuilder.Build();
+                host.RunAsync();
+            }
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            try
+            {
+                // string workingDirectory = Path.GetDirectoryName(Utility.GetExecutingAssemblyLocation());
+                // Directory.SetCurrentDirectory(workingDirectory);
+
+                var builder = new HostBuilder()
+                    .UseNLog()
+                    .ConfigureAppConfiguration((hostingContext, config) =>
+                    {
+                        config.SetBasePath(Directory.GetCurrentDirectory());
+                        config.AddJsonFile("appsettings.json", optional: true);
+                        config.AddJsonFile(
+                            $"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", 
+                            optional: true);
+                        config.AddCommandLine(args);
+                    })
+                    .ConfigureServices((hostContext, services) =>
+                    {
+                        services.AddHostedService<Services.SampleService>();
+                        services.Configure<HostOptions>(option =>
+                        {
+                            option.ShutdownTimeout = System.TimeSpan.FromSeconds(20);
+                        });
+                    })
+                    .ConfigureLogging((hostingContext, logging) => 
+                    {
+                        logging.ClearProviders();
+                        logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                        logging.AddConsole();
+                    });
+
+                return builder;
+            }
+            catch { }
+
+            return null;
         }
     }
 }
